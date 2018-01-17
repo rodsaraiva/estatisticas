70c9e88fc1fcdfe5b85393cb4fc73b01981ac25d
Rodrigo Saraiva <rassaraiva@gmail.com>
2018-01-17 19:09:13 -0200

    aplicando bootstrap

--- src/main/java/com/github/rodsaraiva/estatisticas/model/Jogador.java
+++ src/main/java/com/github/rodsaraiva/estatisticas/model/Jogador.java
@@ -1,6 +1,8 @@
 package com.github.rodsaraiva.estatisticas.model;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -13,6 +15,7 @@ public class Jogador {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	
+	//valor unico
 	private String psn;
 	
 	private String email;
@@ -23,7 +26,10 @@ public class Jogador {
 	@ManyToOne
 	private Time time;
 	
+	@Enumerated(EnumType.STRING)
 	private Posicao posicao1;
+	
+	@Enumerated(EnumType.STRING)
 	private Posicao posicao2;
 	
 
